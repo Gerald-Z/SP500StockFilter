f8cc1ade8ca612113da22483a90cf11f1641b318
Gerald-Z <geraldjialeizhu@gmail.com>
2024-03-30 20:23:59 -0400

    Added Google authentication and profile dropdown bar for navigating to pages.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,33 +1,104 @@
 "use client";
 
-
 import React from 'react'
 import Link from 'next/link';
 import Image from 'next/image';
-
+import { useState, useEffect } from 'react';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
+// import useWindowSize from "@rooks/use-window-size"
 
 
 const Nav = () => {
+    const { data: session} = useSession();
+    const [providers, setProviders] = useState(null);
+  //  const { innerWidth, innerHeight, outerHeight, outerWidth } = useWindowSize();
+    const [profileDropdown, setProfileDropdown] = useState(false);
+
+    useEffect(() => {
+        // Allows users to sign in using Google and Next-auth.
+        const setUpProviders = async () => {
+            const response = await getProviders();
+
+            setProviders(response); 
+        }
+        setUpProviders();
+    }, [])
+
   return (
-    <nav className="flex flex-row w-full mb-16 bg-gray-300 ">
-        <Link href='/' className="gap-2 flex-auto">
+    <nav className="flex flex-row justify-between w-full mb-16 bg-black ">
+        <Link href='/' className="flex-initial basis-1/8">
             <Image 
                 src="/images/sp500.png" 
                 alt="SP500 Logo" 
-                width={200} 
+                width={110} 
                 height={150} 
                 className="object-contain" 
-                
             />
         </Link>
-        <div className="flex-auto flex justify-center items-center pl-10">
-            <p className="font-mono font-extrabold">SP500 Stock Filter</p>
+        <div className="flex-initial basis-6/8 flex justify-center items-center w-300">
+            <p className="font-mono font-extrabold text-white">SP500 Stock Filter</p>
         </div>
-        <div className="flex-auto flex justify-end items-center pr-10">
-            <p className="font-mono font-extrabold py-10">User</p>
+        <div className="flex-initial basis-1/8 flex justify-center items-center pr-10 pl-10 bg-zinc-100	">
+        {session?.user ? (
+            <div className="flex">
+                <Image 
+                    src={session?.user.image}
+                    width={37}
+                    height={37}
+                    className="rounded-full"
+                    alt="profile"
+                    onClick={() => setProfileDropdown((state) => !state)}
+                />
+
+                {profileDropdown 
+                ?    (<div className='profileDropdown'>
+                        <Link href='/' 
+                            className="dropDownLink"                             
+                            onClick={() => setProfileDropdown(false)}>
+                                Homepage   
+                        </Link>
+                        <Link href='/profile' 
+                            className="dropDownLink"                             
+                            onClick={() => setProfileDropdown(false)}>
+                                My Profile    
+                        </Link>
+                        <Link href='/watchlist' 
+                            className="dropDownLink"                             
+                            onClick={() => setProfileDropdown(false)}>
+                                My Watchlist    
+                        </Link>
+                        <Link href='/portfolio' 
+                            className="dropDownLink"                             
+                            onClick={() => setProfileDropdown(false)}>
+                                My Portfolio    
+                        </Link>
+                        <button
+                            onClick={signOut}
+                            className="dropDownLink"      
+                        >
+                            Sign Out
+                        </button>
+                    
+                    </div>)
+                : <></>
+                }
+            </div>
+        ) : 
+            <>
+                {providers && 
+                Object.values(providers).map((provider) => (
+                    <button
+                        type="button"
+                        key={provider.name}
+                        onClick={() => signIn(provider.id)}
+                        className='blackBtn'
+                    >
+                        Sign In 
+                    </button>
+                ))}
+            </>
+        }
         </div>
-
-
     </nav>
   )
 }
